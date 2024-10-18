ed78880240041467bb8177e59cf0d4c2178f9f60
INU-coder <tothel56@gmail.com>
2024-10-19 03:15:18 +0900

    케릭터,플레이어 DB구축
    케릭터 DB에는 데이터까지 구축,
    케릭터 DB에서 데미지,디펜스 등 명칭 수정

---
+++ createDBdata.js
@@ -0,0 +1,53 @@
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
+// import express from 'express';
+// user 1.level * 케릭터 stats
+async function main() {
+  // name    String @unique
+  // health  Int
+  // demage  Int
+  // defence Int
+  // speed   Int
+  await prisma.character.createMany({
+    data: [
+      {
+        name: 'Defender',
+        health: 150,
+        power: 50,
+        defence: 100,
+        speed: 20,
+      },
+      {
+        name: 'Slayer',
+        health: 100,
+        power: 80,
+        defence: 50,
+        speed: 30,
+      },
+      {
+        name: 'Healer',
+        health: 90,
+        power: 40,
+        defence: 40,
+        speed: 40,
+      },
+      {
+        name: 'Archer',
+        health: 80,
+        power: 70,
+        defence: 30,
+        speed: 50,
+      },
+    ],
+  });
+  console.log('케릭터 테이블 데이터 생성');
+}
+
+main()
+  .catch((e) => {
+    console.error(e, 'error');
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
